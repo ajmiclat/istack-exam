cf63e9e51e0ff36d529dd4fbb66980a436e3ec1e
Albert Joseph Miclat <albertjmiclat@gmail.com>
2016-06-16 21:44:22 +0800

    added smooth scroll

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,4 +7,17 @@ $(document).ready(function () {
     projectImg.attr('src', PATH + img);
     projectModal.modal('show');
   });
+
+  $('header a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
